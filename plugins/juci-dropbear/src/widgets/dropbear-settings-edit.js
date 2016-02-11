d17ca2a6040b575d0240167848f60ba0441db163
Sukru Senli <sukru.senli@inteno.se>
2016-02-11 17:07:51 +0100

    add authors to copyright block

--- plugins/juci-dropbear/src/widgets/dropbear-settings-edit.js
+++ plugins/juci-dropbear/src/widgets/dropbear-settings-edit.js
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2015 Inteno Broadband Technology AB. All rights reserved.
  *
+ * Author: Stefan Nygren <stefan.nygren@hiq.se>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
