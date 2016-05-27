34a76234eacf89de90507f981eb27acb30978640
Christopher Nagy <christopher.nagy@inteno.se>
2016-05-27 11:25:50 +0200

    wifi MAC filter page remade, bugs fixed

--- plugins/juci-broadcom-wl/src/widgets/uci.wireless.interface.macfilter.edit.js
+++ plugins/juci-broadcom-wl/src/widgets/uci.wireless.interface.macfilter.edit.js
@@ -29,7 +29,43 @@ JUCI.app
 	}; 
 }).controller("uciWirelessInterfaceMacfilterEditController", function($scope, $uci, $wireless){
 	$scope.maclist = []; 
-	
+	$scope.$watch("interface", function(i){
+		if(i.maclist && i.maclist.value){
+			i.maclist.value.map(function(macaddr){
+				$scope.maclist.push({macaddr:macaddr});
+			});
+		}
+	});
+
+	$scope.$watch("maclist", function(maclist){
+		if(maclist.length == 0){ return; }
+
+		var validMACs = $scope.maclist.filter(isValidMACAddress);
+		maclist.map(function(mac){ mac.error = null; });
+		validMACs.map(function(mac){ mac.error = "Valid MAC Address"; });
+
+		if(isUnAddedMAC(validMACs)){
+			addrs = validMACs.map(function(mac){ return mac.macaddr; });
+			$scope.interface.maclist.value = addrs;
+		}
+	},true);
+
+	function isUnAddedMAC(lst){
+		for(var i=0; i<lst.length; i++){
+			if($scope.interface.maclist.value.indexOf(lst[i].macaddr) == -1){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function isValidMACAddress(mac){
+		if(mac.macaddr.match(/([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}/) === null){ return false; }
+		if(mac.macaddr.length>17){ return false; }
+		return true;
+	}
+
+/*	
 	// watch for model change
 	$scope.$watch("interface", function(i){
 		$scope.maclist = []; 
@@ -49,6 +85,13 @@ JUCI.app
 			//$scope.$apply();  
 		}
 	}); 
+
+	$scope.$watch("maclist", function(){
+		if(!$scope.maclist){ return; }
+		alert("MACLIST");
+		console.log($scope);
+
+	},true);
 	
 	// watch maclist for changes by the user
 	$scope.rebuildMacList = function(){
@@ -77,16 +120,8 @@ JUCI.app
 			$scope.interface.maclist.value = newlist;  
 		}
 	}; 
+	*/
 	
-	$wireless.getConnectedClients().done(function(clients){
-		$scope.client_list = clients.map(function(x){ 
-			return {
-				checked: false, 
-				client: { hostname: x.hostname, macaddr: x.macaddr }
-			}
-		});
-		$scope.$apply(); 
-	}); 
 	
 	$scope.onDeleteHost = function(host){
 		$scope.maclist = ($scope.maclist||[]).filter(function(x) { 
@@ -99,28 +134,39 @@ JUCI.app
 	}
 	
 	$scope.onAddClients = function(){
+		$wireless.getConnectedClients().done(function(clients){
+			$scope.client_list = clients.map(function(x){ 
+				return {
+					checked: false, 
+					client: { hostname: x.hostname, macaddr: x.macaddr }
+				}
+			});
+			$scope.$apply(); 
+			console.log($scope.client_list);
+		}); 
 		// reset all checkboxes 
-		if($scope.client_list){
-			$scope.client_list.map(function(x){ x.checked = false; }); 
-		}
+		//if($scope.client_list){
+		//	$scope.client_list.map(function(x){ x.checked = false; }); 
+		//}
 		$scope.showModal = 1; 
 	}
 	
 	$scope.onAddNewClient = function(){
-		$scope.maclist.push({ hostname: "", macaddr: "" }); 
+		$scope.maclist.push({ macaddr: "" }); 
 	}
 	
 	$scope.onAcceptModal = function(){
 		if($scope.client_list && $scope.maclist) {
 			$scope.client_list.map(function(x){
-				if(x.checked) {
-					if($scope.maclist.filter(function(a) { return a.macaddr == x.client.macaddr; }).length == 0){
-						$scope.maclist.push({ hostname: x.client.hostname, macaddr: x.client.macaddr }); 
-						$scope.rebuildMacList(); 
-					} else {
-						console.log("MAC address "+x.client.macaddr+" is already in the list!"); 
-					}
-				}
+				$scope.maclist.push(x.client);
+				//if(x.checked) {
+				//	if($scope.maclist.filter(function(a) { return a.macaddr == x.client.macaddr; }).length == 0){
+				//		$scope.maclist.push({ macaddr: x.client.macaddr }); 
+				//		$scope.rebuildMacList(); 
+				//	} else {
+				//		console.log("MAC address "+x.client.macaddr+" is already in the list!"); 
+				//	}
+				//}
 			}); 
 		}
 		$scope.showModal = 0; 
