c6b3c66ac7ff288dd53b0a230785b3249aae650d
Sukru Senli <sukru.senli@inteno.se>
2016-05-27 20:25:23 +0200

    dect object already is giving in hex format

--- plugins/juci-natalie-dect/src/pages/voice-dect.js
+++ plugins/juci-natalie-dect/src/pages/voice-dect.js
@@ -64,7 +64,7 @@ JUCI.app
 
 	$scope.toHexString = function(byteArray){
 		return byteArray.map(function(byte) {
-			return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+			return ('0' + byte).slice(-2);
 		}).join('')
 	}
 });
