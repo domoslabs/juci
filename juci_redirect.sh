db425040c0c622baab0501c5e62be49abda0293c
Sukru Senli <sukru.senli@inteno.se>
2016-06-01 11:15:04 +0200

    fix juci_redirect script

--- juci_redirect.sh
+++ juci_redirect.sh
@@ -1,8 +1,8 @@
-#!/usr/sbin/bash
+#!/bin/sh
 echo "Content-type: text/html"
 echo
 echo "<html><head></head>"
 echo "<body>"
 echo "<h1>Please clear your cache and press the button below</h1>"
 echo "<button><a href="/">Click here to go to new JUCI webgui.</a></button>"
-echo "</body></html>" 
+echo "</body></html>"
