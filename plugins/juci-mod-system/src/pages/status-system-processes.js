6f99cab6c1f6b6b6201941e63b630903e5a61896
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-07-04 11:33:26 +0200

    fixed processes so they work with S N and touched up the page a bit

--- plugins/juci-mod-system/src/pages/status-system-processes.js
+++ plugins/juci-mod-system/src/pages/status-system-processes.js
@@ -22,17 +22,30 @@ JUCI.app
 .controller("StatusSystemProcesses", function ($scope, $rpc) {
 	JUCI.interval.repeat("juci-process-list", 5000, function(done){
 		$rpc.$call("juci.system.process", "run", {"method":"list"}).done(function(processes){
-			$scope.processes = processes.list; 
-			$scope.columns = processes.fields; 
+			$scope.processes = processes.list.map(function(l){ l["VSZP"] = l["%VSZ"]; delete l["%VSZ"]; l["CPU"] = l["%CPU"]; delete l["%CPU"]; return l;});
+			$scope.columns = processes.fields.map(function(c){ if(c === "%VSZ") return "VSZP"; else if(c === "%CPU") return "CPU"; else return c;});
+			if(!$scope.order && $scope.columns && $scope.columns.length) $scope.order = $scope.columns[0];
 			$scope.$apply(); 
 			done(); 
 		});
 	}); 
+	$scope.reverse = false;
+	$scope.orderFunction = function(v1, v2){
+		if(v1.match(/^[0-9]+$/) && v2.match(/^[0-9]+$/))
+			return parseInt(v1) < parseInt(v2);
+		else return v1 < v2;
+	}
+	$scope.setOrder = function(col){
+		if($scope.order && $scope.order === col)
+			$scope.reverse = !$scope.reverse;
+		else
+			$scope.order = col.replace(/%/g, "");
+	}
 	$scope.isopen = false;
 	$scope.getCpuUsage = function(){
 		if(!$scope.processes) return '0%'
 		var sum = 0;
-		$scope.processes.map(function(x){sum += Number(x["%CPU"].slice(0, -1));});
+		$scope.processes.map(function(x){sum += Number(x["CPU"].slice(0, -1));});
 		return sum + '%'
 	};
 }); 
