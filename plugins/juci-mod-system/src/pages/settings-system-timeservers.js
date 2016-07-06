8315eb459a0da6e9bb90ae08bb791e93b8a9b2e8
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-07-06 12:12:17 +0200

    split up settings system page into system settings and timeservers and added log settings page

--- plugins/juci-mod-system/src/pages/settings-system-timeservers.js
+++ plugins/juci-mod-system/src/pages/settings-system-timeservers.js
@@ -19,33 +19,25 @@
  */
 
 JUCI.app
-.directive("systemNtpSettingsEdit", function(){
-	return {
-		templateUrl: "/widgets/system-ntp-settings-edit.html", 
-		scope: {
-			ngModel: "=ngModel"
-		}, 
-		controller: "systemNtpSettingsEdit", 
-		replace: true
-	};  
-})
-.controller("systemNtpSettingsEdit", function($scope, $uci){
+.controller("SettingsSystemTimeservers", function($scope, $rpc, $uci, $tr, gettext){
 	$uci.$sync("system").done(function(){
 		if(!$uci.system.ntp) return; 
-		$scope.ntp = $uci.system.ntp.server.value.map(function(x){ return { server: x }; }); 
+		$scope.ntp = $uci.system.ntp;
+		$scope.ntp_servers = $uci.system.ntp.server.value.map(function(x){ return { server: x }; }); 
 		$scope.$apply(); 
-		$scope.$watch("ntp", function(){
+		$scope.$watch("ntp_servers", function(){
 			$uci.system.ntp.server.value = []; 
-			$scope.ntp.map(function(ntp){
+			$scope.ntp_servers.map(function(ntp){
 				$uci.system.ntp.server.value.push(ntp.server); 
 			}); 
 		}, true); 
+		$scope.onDeleteNTPServer = function(ntp){
+			$scope.ntp_servers = $scope.ntp_servers.filter(function(x){ return x != ntp; }); 
+		}
+		$scope.onAddNTPServer = function(){
+			if(!$uci.system.ntp) return; 
+			$scope.ntp_servers.push({ server: "" }); 
+		}
 	}); 
-	$scope.onDeleteNTPServer = function(ntp){
-		$scope.ntp = $scope.ntp.filter(function(x){ return x != ntp; }); 
-	}
-	$scope.onAddNTPServer = function(){
-		if(!$uci.system.ntp) return; 
-		$scope.ntp.push({ server: "" }); 
-	}
-}); 
+});
+
--- plugins/juci-mod-system/src/system.js
+++ plugins/juci-mod-system/src/system.js
@@ -16,34 +16,38 @@
  * 02110-1301 USA
  */
 
-UCI.$registerConfig("system"); 
+UCI.$registerConfig("system");
 
 UCI.system.$registerSectionType("system", {
-	"hostname":		{ dvalue: '', type: String, required: true},
-	"timezone":		{ dvalue: '', type: String },
-	"zonename":		{ dvalue: '', type: String },
+	"hostname":			{ dvalue: '', type: String, required: true},
+	"timezone":			{ dvalue: '', type: String },
+	"zonename":			{ dvalue: '', type: String },
 	"conloglevel":		{ dvalue: 7, type: Number },
 	"cronloglevel":		{ dvalue: 5, type: Number },
-	"log_size":		{ dvalue: 16, type: Number },
-	"log_file": 	{ dvalue: "", type: String },
-	"log_ip": 		{ dvalue: "", type: String },
-	"log_port": 	{ dvalue: undefined, type: Number },
-	"log_prefix": 	{ dvalue: "", type: String }, 
-	"log_remote": 	{ dvalue: false, type: Boolean }
-}); 
+	"klogconloglevel":	{ dvalue: 7, type: Number },
+	"log_file": 		{ dvalue: "", type: String },
+	"log_ip": 			{ dvalue: "", type: String, validator: UCI.validators.IPAddressValidator },
+	"log_port": 		{ dvalue: 514, type: Number },
+	"log_prefix": 		{ dvalue: "", type: String },
+	"log_proto": 		{ dvalue: "udp", type: String },
+	"log_remote": 		{ dvalue: true, type: Boolean },
+	"log_size":			{ dvalue: 16, type: Number },
+	"log_trailer_null":	{ dvalue: false, type: Boolean },
+	"log_type":			{ dvalue: "circular", type: String }
+});
 
 UCI.system.$registerSectionType("timeserver", {
-	"enable_server": { dvalue: false, type: Boolean }, 
+	"enable_server": { dvalue: false, type: Boolean },
 	"server": { dvalue: [], type: Array }
-}); 
+});
 
 UCI.system.$registerSectionType("upgrade", {
 	"fw_check_url":		{ dvalue: "", type: String, required: false},
 	"fw_path_url":		{ dvalue: "", type: String },
-	"fw_usb_path": 		{ dvalue: "", type: String }, 
+	"fw_usb_path": 		{ dvalue: "", type: String },
 	"fw_find_ext":		{ dvalue: ".y", type: String, required: false},
 	"fw_upload_path":	{ dvalue: "", type: String, required: false}
-}); 
+});
 
 UCI.$registerConfig("rpcd");
 
@@ -67,53 +71,53 @@ UCI.leds.$registerSectionType("led", {
 JUCI.app.factory("$systemService", function($rpc){
 	return {
 		list: function(){
-			var def = $.Deferred(); 
+			var def = $.Deferred();
 			$rpc.$call("juci.system.service", "run", {"method":"list"}).done(function(res){
 				if(res && res.services){
 					var result = res.services.map(function(service){
 						service.enable = function(){
-							var self = this; 
-							console.log("enabling service "+self.name); 
-							return $rpc.$call("juci.system.service", "run", {"method":"enable","args":JSON.stringify({ name: self.name })}).done(function(){ self.enabled = true; }); 
+							var self = this;
+							console.log("enabling service "+self.name);
+							return $rpc.$call("juci.system.service", "run", {"method":"enable","args":JSON.stringify({ name: self.name })}).done(function(){ self.enabled = true; });
 						}
 						service.disable = function(){
-							var self = this; 
-							console.log("disabling service "+self.name); 
+							var self = this;
+							console.log("disabling service "+self.name);
 							return $rpc.$call("juci.system.service", "run", {"method":"disable","args":JSON.stringify({ name: self.name })}).done(function(){ self.enabled = false; });
 						}
 						service.start = function(){
-							var self = this; 
-							console.log("starting service "+self.name); 
-							return $rpc.$call("juci.system.service", "run", {"method":"start","args":JSON.stringify({ name: self.name })}).done(function(){ self.running = true; }); 
+							var self = this;
+							console.log("starting service "+self.name);
+							return $rpc.$call("juci.system.service", "run", {"method":"start","args":JSON.stringify({ name: self.name })}).done(function(){ self.running = true; });
 						}
 						service.stop = function(){
-							var self = this; 
-							console.log("stopping service "+self.name); 
-							return $rpc.$call("juci.system.service", "run", {"method":"stop","args":JSON.stringify({ name: self.name })}).done(function(){ self.running = false; }); 
+							var self = this;
+							console.log("stopping service "+self.name);
+							return $rpc.$call("juci.system.service", "run", {"method":"stop","args":JSON.stringify({ name: self.name })}).done(function(){ self.running = false; });
 						}
 						service.reload = function(){
-							var self = this; 
-							return $rpc.$call("juci.system.service", "run", {"method":"reload","args":JSON.stringify({ name: self.name })}); 
+							var self = this;
+							return $rpc.$call("juci.system.service", "run", {"method":"reload","args":JSON.stringify({ name: self.name })});
 						}
 						return service;	
-					}); 
-					def.resolve(result); 
+					});
+					def.resolve(result);
 				} else {
-					def.reject(); 
+					def.reject();
 				}
-			}).fail(function(){ def.reject(); }); 
-			return def.promise(); 
+			}).fail(function(){ def.reject(); });
+			return def.promise();
 		},
 		find: function(name){
-			var def = $.Deferred(); 
+			var def = $.Deferred();
 			this.list().done(function(services){
 				if(services) {
-					def.resolve(services.find(function(x){ return x.name == name; })); 
+					def.resolve(services.find(function(x){ return x.name == name; }));
 				} else {
-					def.reject(); 
+					def.reject();
 				}
-			}).fail(function(){ def.reject(); }); 
-			return def.promise(); 
+			}).fail(function(){ def.reject(); });
+			return def.promise();
 		}
-	}; 
-}); 
+	};
+});
