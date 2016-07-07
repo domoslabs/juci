1721586e5c459f2a3b9fd5e9154e4c30ac56543e
Christopher Nagy <christopher.nagy@inteno.se>
2016-07-07 13:56:22 +0200

    fixed interface selection for QoS

--- plugins/juci-inteno-qos/src/widgets/add-bw-interface-edit.js
+++ plugins/juci-inteno-qos/src/widgets/add-bw-interface-edit.js
@@ -7,6 +7,17 @@ JUCI.app
 			newIface: "=ngModel"
 		},
 		replace: true,
-		templateUrl: "/widgets/add-bw-interface-edit.html"
+		templateUrl: "/widgets/add-bw-interface-edit.html",
+		controller: "addBwInterfaceEdit"
 	};
+})
+.controller("addBwInterfaceEdit", function($scope, $uci, $network){
+	$uci.$sync("qos").done(function(){
+		$network.getNetworks().done(function(nets){
+			function getLabelValue(x){ return {label:x[".name"].toUpperCase(), value:x[".name"].toUpperCase()}; }
+			function isNotLAN(x){ return !(x.value === "LAN"); }
+			$scope.nonLANInterfaces = nets.map(getLabelValue).filter(isNotLAN);
+			$scope.$apply();
+		});
+	});
 });
