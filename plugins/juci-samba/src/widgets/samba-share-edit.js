b1edb9e8e0f1bd709b483a62a1608672cf45bf3a
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-08-17 10:39:27 +0200

    fixed bug where you could type /.. and set anything under / as shared folder

--- plugins/juci-samba/src/widgets/samba-share-edit.js
+++ plugins/juci-samba/src/widgets/samba-share-edit.js
@@ -84,6 +84,7 @@ JUCI.app
 			if(!def){
 				def = $.Deferred(); 
 				$scope.loadingLocations = true;
+				query = query.replace(/\/\.\./g,"");
 				$rpc.$call("juci.samba", "run", {"method":"autocomplete","args":JSON.stringify({ path: query.slice(1) })}).done(function(result){
 					def.resolve(result.folders); 
 				}).fail(function(){
