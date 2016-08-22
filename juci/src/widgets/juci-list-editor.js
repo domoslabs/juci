d7c761afd471cb92ff41e53163be1f15e972770d
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-08-22 09:49:29 +0200

    fixed all errors when hidign item in list-editor

--- juci/src/widgets/juci-list-editor.js
+++ juci/src/widgets/juci-list-editor.js
@@ -17,60 +17,59 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
  */
- 
+
 JUCI.app
 .directive("juciListEditor", function(){
 	return {
 		scope: {
-			items: "=ngItems", 
+			items: "=ngItems",
 			showIcon: "=",
 			iconStatus: "&",
-			editor: "@itemEditor", 
-			editable: "@allowEdit", 
+			editor: "@itemEditor",
+			editable: "@allowEdit",
 			sortable: "@sortable",
-			getItemTitle: "&getItemTitle", 
-			onCreate: "&onCreate", 
-			onDelete: "&onDelete", 
-			onUpdate: "&onUpdate", 
+			getItemTitle: "&getItemTitle",
+			onCreate: "&onCreate",
+			onDelete: "&onDelete",
+			onUpdate: "&onUpdate",
 			onEditStart: "&onEditStart",
 			onItemMoved: "&onItemMoved",
 			hideButtons: "@hideButtons"
-		}, 
-		controller: "juciListEditor", 
-		templateUrl: "/widgets/juci-list-editor.html", 
+		},
+		controller: "juciListEditor",
+		templateUrl: "/widgets/juci-list-editor.html",
 		compile: function(element, attrs){
 		   if (!attrs.allowEdit) { attrs.allowEdit = true; }
 		   if (attrs.allowEdit == "false") { attrs.allowEdit = false; }
 		}
-	}; 
+	};
 })
 .controller("juciListEditor", function($scope){
-	$scope.item = null;
-	$scope.dynamicHtml = "<"+$scope.editor+" ng-model='item'/>"; 
+	$scope.hide = true;
+	$scope.dynamicHtml = "<"+$scope.editor+" ng-model='item'/>";
 	$scope.onListAddItem = function(){
-		$scope.item = null; 
 		$scope.onCreate();
 	}
 	$scope.hideEditor = function(){
-		$scope.item = null;
+		$scope.hide = true;
 	}
 	$scope.onListEditItem = function(i){
-		console.log("edit item");
-		$scope.item = i; 
-		$scope.onEditStart({"$item": i}); 
+		$scope.item = i;
+		$scope.hide = false;
+		$scope.onEditStart({"$item": i});
 	}
 	$scope.onListRemoveItem = function(i){
-		$scope.onDelete({"$item": i});  
-		$scope.item = null; //$scope.items.find(function(x){ return x == i }); 
+		$scope.onDelete({"$item": i});
+		$scope.hide = true;
 	}
 	$scope.onMoveUp = function(i){
-		var arr = $scope.items; 
-		var idx = arr.indexOf(i); 
+		var arr = $scope.items;
+		var idx = arr.indexOf(i);
 		// return if either not found or already at the top
-		if(idx == -1 || idx == 0) return; 
-		arr.splice(idx, 1); 
-		arr.splice(idx - 1, 0, i); 
-		$scope.onItemMoved({ $item: i, $prev_index: idx, $index: idx - 1}); 
+		if(idx == -1 || idx == 0) return;
+		arr.splice(idx, 1);
+		arr.splice(idx - 1, 0, i);
+		$scope.onItemMoved({ $item: i, $prev_index: idx, $index: idx - 1});
 	}
 	$scope.getIcon = function(iconStatus){
 		if(iconStatus == "muted") 	return "fa fa-circle fa-2x text-muted";
@@ -86,12 +85,12 @@ JUCI.app
 	};
 
 	$scope.onMoveDown = function(i){
-		var arr = $scope.items; 
-		var idx = arr.indexOf(i); 
+		var arr = $scope.items;
+		var idx = arr.indexOf(i);
 		// return if either not found or already at the bottom
 		if(idx == -1 || idx == arr.length - 1) return;
-		arr.splice(idx, 1); 
-		arr.splice(idx + 1, 0, i); 
-		$scope.onItemMoved({ $item: i, $prev_index: idx, $index: idx + 1}); 
+		arr.splice(idx, 1);
+		arr.splice(idx + 1, 0, i);
+		$scope.onItemMoved({ $item: i, $prev_index: idx, $index: idx + 1});
 	}
-}); 
+});
