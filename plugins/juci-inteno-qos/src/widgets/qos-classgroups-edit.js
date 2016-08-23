5917882386d5e5c5212f624b0ab01a285b016f43
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-08-23 14:55:16 +0200

    fixed error and fixed css

--- plugins/juci-inteno-qos/src/widgets/qos-classgroups-edit.js
+++ plugins/juci-inteno-qos/src/widgets/qos-classgroups-edit.js
@@ -11,43 +11,46 @@ JUCI.app
 	};
 })
 .controller("qosClassgroupsCtrl", function($scope, $uci, $tr){
-	$uci.$sync("qos").done(function(){
+	$scope.$watch("instance", function(){
+		if(!$scope.instance) return;
+		$uci.$sync("qos").done(function(){
 
-		$scope.dummy = {value:""};
-		$scope.addedClasses = $scope.instance.classes.value.map(function(c){
-			return { value: c, label: c };
-		});
-		$scope.allClasses = $uci.qos["@class"].map(function(c){
-			return { value: c[".name"], label: c[".name"] };
-		});
-		var addedClassNames = $scope.addedClasses.map(function(x){ return x.value; });
-		$scope.unAddedClasses = $scope.allClasses.filter(function(x){
-			return addedClassNames.indexOf(x.value) === -1;
-		});
-
-		$scope.delClassFromGroup = function(className){ 
-			$scope.instance.classes.value = $scope.instance.classes.value.filter(function(c){
-				return c !== className
-			});
-		};
-		$scope.addClassToGroup = function(className){
-			$scope.instance.classes.value = $scope.instance.classes.value.concat([className]);
-		};
-
-		$scope.reloadClasses = function(){
+			$scope.dummy = {value:""};
 			$scope.addedClasses = $scope.instance.classes.value.map(function(c){
 				return { value: c, label: c };
 			});
 			$scope.allClasses = $uci.qos["@class"].map(function(c){
 				return { value: c[".name"], label: c[".name"] };
 			});
-
 			var addedClassNames = $scope.addedClasses.map(function(x){ return x.value; });
 			$scope.unAddedClasses = $scope.allClasses.filter(function(x){
 				return addedClassNames.indexOf(x.value) === -1;
 			});
-		}
 
-		$scope.$apply();
-	});
+			$scope.delClassFromGroup = function(className){
+				$scope.instance.classes.value = $scope.instance.classes.value.filter(function(c){
+					return c !== className
+				});
+			};
+			$scope.addClassToGroup = function(className){
+				$scope.instance.classes.value = $scope.instance.classes.value.concat([className]);
+			};
+
+			$scope.reloadClasses = function(){
+				$scope.addedClasses = $scope.instance.classes.value.map(function(c){
+					return { value: c, label: c };
+				});
+				$scope.allClasses = $uci.qos["@class"].map(function(c){
+					return { value: c[".name"], label: c[".name"] };
+				});
+
+				var addedClassNames = $scope.addedClasses.map(function(x){ return x.value; });
+				$scope.unAddedClasses = $scope.allClasses.filter(function(x){
+					return addedClassNames.indexOf(x.value) === -1;
+				});
+			}
+
+			$scope.$apply();
+		});
+	}, false);
 });
