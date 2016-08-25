domoslabs/juci

commit 641f7045ff70a6f44b7f02ec183901693b937df9

added a check to see if the session is valid befor leaving a page and fixed sfp-status page info and added info to catv-status page in case there is no status avalible

juci/src/js/juci.js
@@ -206,10 +206,6 @@
 					},
 					// this function will run upon load of every page in the gui
 					onEnter: function($uci, $window, $rootScope, $tr, gettext){
-						if(page.redirect) {
-							$juci.redirect(page.redirect);
-							return;
-						}
 						
 						$rootScope.errors.splice(0, $rootScope.errors.length);
 						
@@ -226,15 +222,20 @@
 						$window.scrollTo(0, 0);
 					},
 					onExit: function($uci, $tr, gettext, $events){
-						if($uci.$hasChanges()){
-							if(confirm($tr(gettext("You have unsaved changes. Do you want to save them before leaving this page?"))))
-								$uci.$save();
-							else
-								$uci.$clearCache();
-						}
-						// clear all juci intervals when leaving a page
-						JUCI.interval.$clearAll();
-						$events.removeAll();
+						$rpc.$authenticate().done(function(){
+							if($uci.$hasChanges()){
+								if(confirm($tr(gettext("You have unsaved changes. Do you want to save them before leaving this page?"))))
+									$uci.$save();
+								else
+									$uci.$clearCache();
+							}
+						}).fail(function(){
+							$juci.redirect("login");
+						}).always(function(){
+							// clear all juci intervals when leaving a page
+							JUCI.interval.$clearAll();
+							$events.removeAll();
+						});
 					}
 				};
 				

plugins/juci-catv/src/pages/status-catv.html
@@ -2,12 +2,13 @@
 	<div ng-controller="statusCatvCtrl">
 		<juci-config-section>
 			<juci-config-heading>{{'CATV Status'|translate}}</juci-config-heading>
-			<table class="table table-hover">
+			<table ng-show="values && values.length" class="table table-hover">
 				<tr ng-repeat="row in values track by $index">
 					<td>{{row.key}}</dt>
 					<td>{{row.value}}</td>
 				</tr>
 			</table>
+			<p ng-hide="values && values.length">{{'No status avalible'|translate}}</p>
 		</juci-config-section>
 	</div>
 </juci-layout-with-sidebar>

plugins/juci-sfp/src/pages/status-sfp.html
@@ -9,7 +9,7 @@
 				<td>{{row.value}}</td>
 			</tr>
 		</table>
-		<h5 ng-show="ddm.length == 0">No DDM Values Available</h5>
+		<h5 ng-show="rom.length == 0">No ROM Values Available</h5>
 		<h3 translate>DDM Values</h3>
 		<table ng-show="rom.length > 0" class="table table-hover">
 			<tr ng-repeat="row in rom track by $index">
@@ -17,6 +17,6 @@
 				<td>{{row.value}}</td>
 			</tr>
 		</table>
-		<h5 ng-show="rom.length == 0">No ROM Values Available</h5>
+		<h5 ng-show="ddm.length == 0">No DDM Values Available</h5>
 	</div>
 </juci-layout-with-sidebar>

commit e17448ea3f249ee76294d4e975be86c50d26acff

fixed bug introduced in 641f7045ff70a6f44b7f02ec183901693b937df9 and lowered the time before redirect to reboot page to 1 sec

juci/src/js/juci.js
@@ -222,6 +222,8 @@
 						$window.scrollTo(0, 0);
 					},
 					onExit: function($uci, $tr, gettext, $events){
+						JUCI.interval.$clearAll();
+						$events.removeAll();
 						$rpc.$authenticate().done(function(){
 							if($uci.$hasChanges()){
 								if(confirm($tr(gettext("You have unsaved changes. Do you want to save them before leaving this page?"))))
@@ -233,8 +235,6 @@
 							$juci.redirect("login");
 						}).always(function(){
 							// clear all juci intervals when leaving a page
-							JUCI.interval.$clearAll();
-							$events.removeAll();
 						});
 					}
 				};

plugins/juci-sysupgrade/src/pages/settings-upgrade.js
@@ -85,7 +85,7 @@ JUCI.app
 		$scope.message = $tr(gettext("Upgrading"));
 		$scope.$apply();
 		$rpc.$call("juci.system.upgrade", "run", {"method":"start","args":JSON.stringify({"path": $scope.$PATH, "keep": (($scope.$KEEP)?1:0)})});
-		setTimeout(function(){ window.location = "/reboot.html";}, 3000);
+		setTimeout(function(){ window.location = "/reboot.html";}, 1000);
 	});
 	$scope.onDismissModal = function(){
 		$scope.showUpgradeStatus = false;