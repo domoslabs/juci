490d58fc5cf3f8f268cfc4a2693520040754046e
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-09-06 09:56:25 +0200

    some minor touchups to dhcp-leases status page

--- plugins/juci-dnsmasq-dhcp/src/widgets/dhcp-leases-widget.js
+++ plugins/juci-dnsmasq-dhcp/src/widgets/dhcp-leases-widget.js
@@ -33,17 +33,15 @@ JUCI.app
 		JUCI.interval.repeat("dhcp-leases", 5000, function(done){refresh();done();});
 	});
 	function refresh(){
-		//lanNets.map(function(net){
-			$rpc.$call("router", "leases", {family:4}).done(function(res){
-				$scope.ipv4leases = Object.keys(res).map(function(k){return res[k];});
-			});
-
-			$rpc.$call("router", "leases", {family:6}).done(function(res){
-				$scope.ipv6leases = Object.keys(res).map(function(k){return res[k];});
-			});
-
+		$rpc.$call("router", "leases", {family:4}).done(function(res){
+			$scope.ipv4leases = Object.keys(res).map(function(k){return res[k];});
 			$scope.$apply();
-		//});
+		});
+
+		$rpc.$call("router", "leases", {family:6}).done(function(res){
+			$scope.ipv6leases = Object.keys(res).map(function(k){return res[k];});
+			$scope.$apply();
+		});
 	}refresh();
 			
 	JUCI.interval.repeat("leases-update-time", 1000, function(done){
