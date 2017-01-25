c6d811b6a572768ddf95018ae18b4cb58e4f3a8d
Christopher Nagy <christopher.nagy@inteno.se>
2017-01-25 18:55:34 +0100

    bandwidth page can set steering policy

--- plugins/juci-wireless/src/pages/wireless-band-steering.js
+++ plugins/juci-wireless/src/pages/wireless-band-steering.js
@@ -1,11 +1,11 @@
 JUCI.app
 .controller("wirelessBandSteering", function($scope, $uci, $wireless){
 	$uci.$sync("wireless").done(function(data){
-		$scope.wireless = $uci.wireless;
+		$scope.bandsteering = $uci.wireless['@bandsteering'][0];
+		$scope.enabled = $scope.bandsteering.enabled.value;
+		$scope.policys = [{label:'RSSI',value:0},{label:'Bandwidth Usage',value:1}];
 		$scope.$apply();
 	});
-
-
 	$rpc.$call("router.wireless", "radios").done(function(data){
 		$scope.wlRadios = data;
 		$scope.isDualBand = function(){
@@ -20,7 +20,4 @@ JUCI.app
 		$scope.isntDualBand = function(){ return !$scope.isDualBand(); };
 		$scope.$apply();
 	});
-
-	
-	
 }); 
