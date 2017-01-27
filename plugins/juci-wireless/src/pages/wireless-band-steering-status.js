5d9d8bbe81dfe3d60fd802817814a957b06e1bf1
Christopher Nagy <christopher.nagy@inteno.se>
2017-01-27 13:45:45 +0100

    Status>WifiStatus>BandSteering: Added status page for bandsteering which shows bsd -s and bsd -l

---
+++ plugins/juci-wireless/src/pages/wireless-band-steering-status.js
@@ -0,0 +1,18 @@
+JUCI.app
+.controller("wirelessBandSteeringStatus", function($scope, $uci, $wireless, gettext, $rpc, $tr, gettext){
+	$scope.data = {};
+	function update(){
+		$rpc.$call("juci.wireless", "bsd_status").done(function(data){
+			$scope.data.bsd_status = data.bsd_status;
+			$scope.$apply();
+		})
+		$rpc.$call("juci.wireless", "bsd_log").done(function(data){
+			$scope.data.bsd_log = data.bsd_log;
+			$scope.$apply();
+		})
+	}
+	JUCI.interval.repeat("update",5000,function(next){
+		update();
+		next();
+	});
+}); 
