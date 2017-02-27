b94f23761f9524b139261aad32fe71106a3ac251
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2017-02-27 12:27:30 +0100

    cleand up dsl-status page

--- plugins/juci-broadcom-dsl/src/pages/page-broadcom-dsl-status.js
+++ plugins/juci-broadcom-dsl/src/pages/page-broadcom-dsl-status.js
@@ -20,121 +20,111 @@
 
 JUCI.app
 .controller("PageBroadcomDslStatus", function($scope, $rpc, gettext, $tr){
-		$scope.dslstats = {};
-		
-		$scope.dslConnectionInfo = {
-			title: "test",
-			rows: [[$tr(gettext("None")), $tr(gettext("None"))]]
-		};
-		$scope.dslModeInfo = {
-			rows: [[$tr(gettext('None')), $tr(gettext("None"))]]
-		};
-		$scope.dslStatusInfo = {
-			rows: []
-		};
-		$scope.dslRateInfo = {
-			rows: []
-		};
-		$scope.dslOpInfo = {
-			rows: []
-		};
-		$scope.dslErrorInfo = {
-			rows: []
-		};
-		$scope.dslCellInfo = {
-			rows: []
-		};
+	JUCI.interval.repeat("dslstatus", 5000, function(done){
+		$rpc.$call("router.dsl", "stats", {}).done(function(dslstats){
+			var dslstats = dslstats.dslstats;
 
-		JUCI.interval.repeat("dslstatus", 5000, function(done){
-			$rpc.$call("router.dsl", "stats", {}).done(function(dslstats){
-				dslstats = dslstats.dslstats;
-				
-				// compute floating point values (because ubus blobs do not support floats yet)
-				function reconstruct_floats(obj) {
-					for (var property in obj) {
-						if (obj.hasOwnProperty(property)) {
-							if (typeof obj[property] == "object") {
-								reconstruct_floats(obj[property]);
-							} else {
-								var matches = property.match(/(.*)_x([\d]*)/);
-								if(matches && matches.length == 3){
-									try {
-										obj[matches[1]] = parseFloat(String(obj[property])) / parseFloat(matches[2]);
-									} catch(e) {
-										obj[matches[1]] = "Err";
-									}
-								}
-							}
+			// compute floating point values (because ubus blobs do not support floats yet)
+			function reconstruct_floats(obj) {
+				for (var property in obj) {
+					if (!obj.hasOwnProperty(property))
+						return;
+					if (typeof obj[property] === "object") {
+						reconstruct_floats(obj[property]);
+					} else {
+						var matches = property.match(/(.*)_x([\d]*)/);
+						if(matches && matches.length === 3){
+							obj[matches[1]] = parseFloat(String(obj[property])) /
+								parseFloat(matches[2]);
 						}
 					}
 				}
-				reconstruct_floats(dslstats);
-				
-				$scope.online = dslstats.status && dslstats.status.length > 0;
-				
-				// todo fields
-				with({dslstats: dslstats}){
-					dslstats.ip = "TODO";
-					dslstats.ipstate = "TODO";
-					dslstats.mode_details = "TODO";
-					dslstats.line_status_configured = "TODO";
-					dslstats.line_type_configured = "TODO";
-					dslstats.line_type = "TODO";
+			}
+			reconstruct_floats(dslstats);
+
+			$scope.tables = [
+				{
+					title: $tr(gettext("DSL Status Information")),
+					columns: [ '', '', 'Current' ],
+					rows: [
+						[
+							$tr(gettext("Line Status")),
+							"",
+							dslstats.status
+						]
+					]
 				}
-				
-				$scope.tables = [
+			];
+			if(dslstats.status === "Showtime"){
+				$scope.tables = $scope.tables.concat([
 					{
-						title: $tr(gettext("DSL Status Information")),
-						columns: [ '', '', 'Current' ],
+						title: $tr(gettext("DSL Mode")),
+						columns: ["", "", "Current"],
 						rows: [
-							[ $tr(gettext("Line Status")), "", dslstats.status ]
+							[
+								dslstats.mode, "",
+								dslstats.traffic
+							]
+						]
+					},
+					{
+						title: $tr(gettext("Bit Rate")),
+						columns: [ '', 'Downstream', 'Upstream' ],
+						rows: [
+							[
+								$tr(gettext('Actual Data Rate')),
+								dslstats.bearers[0].rate_down,
+								dslstats.bearers[0].rate_up
+							]
+						]
+					},
+					{
+						title: $tr(gettext("Operating Data")),
+						columns: [ '', 'Downstream', 'Upstream' ],
+						rows: [
+							[
+								$tr(gettext('SNR Margin')),
+								dslstats.snr_down,
+								dslstats.snr_up
+							],
+							[
+								$tr(gettext('Loop Attenuation')),
+								dslstats.attn_down,
+								dslstats.attn_up
+							]
+						]
+					},
+					{
+						title: $tr(gettext("Error Counter")),
+						columns: [ '', 'Downstream', 'Upstream' ],
+						rows: [
+							[
+								$tr(gettext('FEC Corrections')),
+								dslstats.counters.totals.fec_down,
+								dslstats.counters.totals.fec_up
+							],
+							[
+								$tr(gettext('CRC Errors')),
+								dslstats.counters.totals.crc_down,
+								dslstats.counters.totals.crc_up
+							]
+						]
+					},
+					{
+						title: $tr(gettext("Cell Statistics")),
+						columns: [ '', 'Transmitted', 'Received' ],
+						rows: [
+							[
+								$tr(gettext('Cell Counter')),
+								dslstats.bearers[0].total_cells_down,
+								dslstats.bearers[0].total_cells_up
+							]
 						]
 					}
-				];
-				if(dslstats.status == "Showtime"){
-					$scope.tables = $scope.tables.concat([
-						{
-							title: $tr(gettext("DSL Mode")),
-							columns: ["", "", "Current"],
-							rows: [
-								[ dslstats.mode, "", dslstats.traffic ]
-							]
-						},
-						{
-							title: $tr(gettext("Bit Rate")),
-							columns: [ '', 'Downstream', 'Upstream' ],
-							rows: [
-								[ $tr(gettext('Actual Data Rate')), dslstats.bearers[0].rate_down, dslstats.bearers[0].rate_up ]
-							]
-						},
-						{
-							title: $tr(gettext("Operating Data")),
-							columns: [ '', 'Downstream', 'Upstream' ],
-							rows: [
-								[ $tr(gettext('SNR Margin')), dslstats.snr_down, dslstats.snr_up ],
-								[ $tr(gettext('Loop Attenuation')), dslstats.attn_down, dslstats.attn_up ]
-							]
-						},
-						{
-							title: $tr(gettext("Error Counter")),
-							columns: [ '', 'Downstream', 'Upstream' ],
-							rows: [
-								[ $tr(gettext('FEC Corrections')), dslstats.counters.totals.fec_down, dslstats.counters.totals.fec_up ],
-								[ $tr(gettext('CRC Errors')), dslstats.counters.totals.crc_down, dslstats.counters.totals.crc_up ]
-							]
-						},
-						{
-							title: $tr(gettext("Cell Statistics")),
-							columns: [ '', 'Transmitted', 'Received' ],
-							rows: [
-								[ $tr(gettext('Cell Counter')), dslstats.bearers[0].total_cells_down, dslstats.bearers[0].total_cells_up ]
-							]
-						}
-					]);
-				}
-				$scope.dslstats = dslstats;
-				$scope.$apply();
-				done();
-			});
+				]);
+			}
+			$scope.$apply();
+			done();
 		});
 	});
+});
