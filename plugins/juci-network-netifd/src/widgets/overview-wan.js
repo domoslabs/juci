9809e8b188ddffd95755ca96112938a9943f3902
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2017-03-16 12:02:06 +0100

    wan-overview-widget: added refresh to run every 10 sec

--- plugins/juci-network-netifd/src/widgets/overview-wan.js
+++ plugins/juci-network-netifd/src/widgets/overview-wan.js
@@ -111,7 +111,9 @@ JUCI.app
 		}
 	}
 	JUCI.interval.repeat("wan_check_internet", 10000, function(next){
-		update_online().always(function(){next();});
+		refresh().always(function(){
+			update_online().always(function(){next();});
+		});
 	});
 
 	function update_online(){
