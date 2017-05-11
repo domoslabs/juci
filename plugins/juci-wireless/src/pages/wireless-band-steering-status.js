4467fb29de7cbb35a77855ba9594457e6663bafa
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2017-05-11 16:55:38 +0200

    juci-wireless: updated bsd-status page to use new backend

--- plugins/juci-wireless/src/pages/wireless-band-steering-status.js
+++ plugins/juci-wireless/src/pages/wireless-band-steering-status.js
@@ -1,18 +1,45 @@
 JUCI.app
 .controller("wirelessBandSteeringStatus", function($scope, $uci, $wireless, gettext, $rpc, $tr, gettext){
 	$scope.data = {};
-	function update(){
-		$rpc.$call("juci.wireless", "bsd_status").done(function(data){
-			$scope.data.bsd_status = data.bsd_status;
-			$scope.$apply();
-		})
-		$rpc.$call("juci.wireless", "bsd_log").done(function(data){
-			$scope.data.bsd_log = data.bsd_log;
-			$scope.$apply();
-		})
-	}
-	JUCI.interval.repeat("update",10000,function(next){
-		update();
-		next();
+	$wireless.getInterfaces().done(function(ifaces){
+		function update(){
+			$rpc.$call("router.wireless", "bsd_sta_info").done(function(data){
+				if(!data || !data.stations || !data.stations instanceof Array)
+					return;
+				$scope.data.bsd_status = data.stations.map(function(sta){
+					sta.sta_mac = String(sta.sta_mac).toUpperCase();
+					var iface = ifaces.find(function(i){
+						return i && i.$info && i.$info.wldev && i.$info.wldev === sta.interface;
+					});
+					if(iface && iface[".frequency"])
+						sta.freq = iface[".frequency"];
+					return sta;
+				});
+				$scope.$apply();
+			}).fail(function(e){
+				console.log(e);
+			});
+			$rpc.$call("router.wireless", "bsd_records").done(function(data){
+				if(!data || !data.logs || !data.logs instanceof Array)
+					return;
+				$scope.data.bsd_logs = data.logs.map(function(log){
+					log.sta_mac = String(log.sta_mac).toUpperCase();
+					return log;
+				});
+				$scope.$apply();
+			}).fail(function(e){
+				console.log(e);
+			});
+		}
+		JUCI.interval.repeat("update",10000,function(next){
+			update();
+			next();
+		});
 	});
-}); 
+})
+.filter('capitalize', function() {
+	return function(input) {
+		input = String(input);
+		return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+	}
+});
