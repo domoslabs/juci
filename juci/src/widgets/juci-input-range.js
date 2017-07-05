b9c81c0f3d49df74f915feb0826deb62acebc77a
Christopher Nagy <christopher.nagy@inteno.se>
2017-07-05 18:09:46 +0200

    juci-input-range: improvements

--- juci/src/widgets/juci-input-range.js
+++ juci/src/widgets/juci-input-range.js
@@ -15,6 +15,7 @@ JUCI.app
 	};
 })
 .controller("juciInputRange", function($scope, $log, $parse, $attrs) {
+console.log("RUNNINT STUFF");
 	$scope.data = {
 		isRange: false,
 		from: "",
@@ -24,30 +25,37 @@ JUCI.app
 	$scope.expandSizeRange = function(){
 		$scope.data.isRange = !$scope.data.isRange;
 	}
-
 	// when the model is changed, update data.from and data.to
 	$scope.$watch("model", function(value){
 		if(!$scope.model){ return; }
-		if(value.indexOf($scope.delimiter) !== -1){ // if model is a range
+		if(String(value).indexOf($scope.delimiter) !== -1){ // if model is a range
+			$scope.data.isRange = true;
 			$scope.data.from = parseInt($scope.model.split($scope.delimiter)[0]);
 			$scope.data.to = parseInt($scope.model.split($scope.delimiter)[1]);
 		}		
+		else{
+			$scope.data.isRange = false;
+			$scope.data.from = parseInt($scope.model);
+		};
 	});
 
 	$scope.$watch("data", function(value){
 		if(!$scope.data){ return; }
-		// if user opened with expand button indicating a range
-		if($scope.data.isRange){
-			if($scope.data.from && $scope.data.from !== ""){
-				if($scope.data.to && $scope.data.to !== ""){
+		if($scope.data.isRange){ // if user opened with expand button indicating a range
+			if($scope.data.from){
+				if($scope.data.to){
 					$scope.model = String($scope.data.from) + $scope.delimiter + String($scope.data.to);
 				}
 			}
 		}
 		else{// if user closed with expand button indicating a single value 
-			if($scope.data.from && $scope.data.from !== ""){
-				$scope.model = String($scope.data.from);
+			if($scope.data.from){
+				$scope.model = $scope.data.from;
 			}
+//TODO: WHEN USER SETS "" SET MODEL TO ""
+			//else if($scope.data.from === ""){
+			//	$scope.model = "";
+			//}
 		}
 	}, true);
 });
