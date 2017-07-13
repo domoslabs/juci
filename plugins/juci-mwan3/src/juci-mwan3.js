f425636758a2d938fbbe7e9bb385a31d90345420
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2017-07-13 11:08:29 +0200

    mwan3: add default values to some config options and added track_type to interface seciton

--- plugins/juci-mwan3/src/juci-mwan3.js
+++ plugins/juci-mwan3/src/juci-mwan3.js
@@ -1,7 +1,8 @@
 UCI.$registerConfig("mwan3");
 UCI.mwan3.$registerSectionType("interface", {
-	"enabled":	{ dvalue: null, type: Boolean },
+	"enabled":	{ dvalue: 0, type: Boolean },
 	"track_ip":	{ dvalue: [], type: Array },
+	"track_type":	{ dvalue: "ip", type: String },
 	"reliability":	{ dvalue: 1, type: Number },
 	"count":	{ dvalue: 1, type: Number },
 	"family":	{ dvalue: "ipv4", type: String },
@@ -20,12 +21,12 @@ UCI.mwan3.$registerSectionType("policy", {
 });
 UCI.mwan3.$registerSectionType("rule", {
 	"use_policy":	{ dvalue: "", type: String },
-	"src_ip":	{ dvalue: "", type: String, validator: UCI.validators.IP4AddressValidator },
+	"src_ip":	{ dvalue: "0.0.0.0/0", type: String, validator: UCI.validators.IP4AddressValidator },
 	"src_port":	{ dvalue: "0:65535", type: String, validator:  UCI.validators.PortsOrRangeValidator() },
 	"proto":	{ dvalue: "all", type: String },
-	"dest_ip":	{ dvalue: "", type: String, validator: UCI.validators.IP4AddressValidator },
-	"dest_port":	{ dvalue: "0:65535", type: String, validator:  UCI.validators.PortsOrRangeValidator() }
+	"dest_ip":	{ dvalue: "0.0.0.0/0", type: String, validator: UCI.validators.IP4AddressValidator },
 	//"ipset":	{ dvalue: "", type: String },
 	//"sticky":	{ dvalue: 0, type: Boolean },
 	//"timeout":	{ dvalue: 600, type: Number }
+	"dest_port":	{ dvalue: "0:65535", type: String, validator:  UCI.validators.PortsOrRangeValidator() }
 });
