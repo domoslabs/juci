58ffbd39cddc73d3ba3314874427d6f236b0085a
Alex Oprea <alex.oprea@inteno.se>
2017-07-27 13:34:02 +0200

    saving more good work

--- plugins/juci-openvpn/src/openvpn.js
+++ plugins/juci-openvpn/src/openvpn.js
@@ -60,6 +60,25 @@ UCI.$registerConfig("openvpn");
 UCI.openvpn.$registerSectionType("openvpn", {
 	"enabled":	{ dvalue: false, type: Boolean },
 	"auth_user":	{ dvalue: "", type: String },
-	"auth_pass": 	{ dvalue: "", type: String },
-	"config":	{ dvalue: "", type: String }
+	"auth_pass":	{ dvalue: "", type: String },
+	"config":	{ dvalue: "", type: String },
+
+	"client":	{ dvalue: true, type: Boolean },
+	"dev":		{ dvalue: "tun", type: String },
+	"proto":	{ dvalue: "udp", type: String },
+	//"remote":	{ dvalue:  '217.27.188.82 1194', type: x }, //LIST
+	"auth":		{ dvalue: "SHA1", type: String },
+	"cipher":	{ dvalue: "BF-CBC", type: String },
+	"nobind":	{ dvalue: true, type: Boolean },
+	"persist_tun":	{ dvalue: "persist-tun", type: String },
+	"persist_key":	{ dvalue: "persist-key", type: String },
+	"verb":		{ dvalue: "1", type: String },
+	"mute":		{ dvalue: "20", type: Number },
+	"mute_replay_warnings":	{ dvalue: true, type: Boolean },
+	"ns_cert_type":	{ dvalue: "", type: String },
+	"comp_lzo":	{ dvalue: "adaptive", type: String },
+	"resolv_retry":	{ dvalue: "infinite", type: String },
+	"ca":		{ dvalue: "/var/etc/openvpn-ca.crt", type: String },
+	"cert":		{ dvalue: "/var/etc/openvpn-client.crt", type: String },
+	"key":		{ dvalue: "/var/etc/openvpn-client.key", type: String }
 });
--- plugins/juci-openvpn/src/pages/internet-services-openvpn.js
+++ plugins/juci-openvpn/src/pages/internet-services-openvpn.js
@@ -26,6 +26,68 @@ JUCI.app
 		passwd_entries: []
 	};
 
+	$scope.dev_list = [
+		{ label: $tr(gettext("tun")), value: "tun" },
+		{ label: $tr(gettext("tap")), value: "tap" }
+	];
+	$scope.proto_list = [
+		{ label: $tr(gettext("udp")), value: "udp" },
+		{ label: $tr(gettext("tcp")), value: "tcp" }
+	];
+	$scope.auth_list = [
+		{ label: $tr(gettext("SHA1")), value: "SHA1" },
+		{ label: $tr(gettext("none")), value: "none" }
+	];
+	$scope.cipher_list = [
+		{ label: $tr(gettext("DES-CBC")),	value: "DES-CBC" },
+		{ label: $tr(gettext("RC2-CBC")),	value: "RC2-CBC" },
+		{ label: $tr(gettext("DES-EDE-CBC")),	value: "DES-EDE-CBC" },
+		{ label: $tr(gettext("DES-EDE3-CBC")),	value: "DES-EDE3-CBC"},
+		{ label: $tr(gettext("DESX-CBC")),	value: "DESX-CBC" },
+		{ label: $tr(gettext("BF-CBC")),	value: "BF-CBC" },
+		{ label: $tr(gettext("RC2-40-CBC")),	value: "RC2-40-CBC" },
+		{ label: $tr(gettext("CAST5-CBC")),	value: "CAST5-CBC" },
+		{ label: $tr(gettext("RC2-64-CBC")),	value: "RC2-64-CBC" },
+		{ label: $tr(gettext("AES-128-CBC")),	value: "AES-128-CBC" },
+		{ label: $tr(gettext("AES-192-CBC")),	value: "AES-192-CBC" },
+		{ label: $tr(gettext("AES-256-CBC")),	value: "AES-256-CBC" },
+		{ label: $tr(gettext("SEED-CBC")),	value: "SEED-CBC" }
+	];
+	$scope.verb_list = [
+		{ label: $tr(gettext("0")), value: "0" },
+		{ label: $tr(gettext("1")), value: "1" },
+		{ label: $tr(gettext("2")), value: "2" },
+		{ label: $tr(gettext("3")), value: "3" },
+		{ label: $tr(gettext("4")), value: "4" },
+		{ label: $tr(gettext("5")), value: "5" },
+		{ label: $tr(gettext("6")), value: "6" },
+		{ label: $tr(gettext("7")), value: "7" },
+		{ label: $tr(gettext("8")), value: "8" },
+		{ label: $tr(gettext("9")), value: "9" },
+		{ label: $tr(gettext("10")), value: "10" },
+		{ label: $tr(gettext("11")), value: "11" }
+	];
+	$scope.ns_cert_type_list = [
+		{ label: $tr(gettext("client")), value: "client" },
+		{ label: $tr(gettext("server")), value: "server" },
+		{ label: $tr(gettext("none")), value: "" }
+	];
+	$scope.comp_lzo_list = [
+		{ label: $tr(gettext("yes")), value: "yes" },
+		{ label: $tr(gettext("no")), value: "no" },
+		{ label: $tr(gettext("adaptive")), value: "adaptive" }
+	];
+
+	$scope.showPassword = true;
+	$scope.togglePassword = function(){
+		$scope.showPassword = !$scope.showPassword;
+	}
+
+	$scope.resolv_retry_infinite = true;
+	$scope.toggle_resolv_retry_infinite = function(){
+		$scope.resolv_retry_infinite = !$scope.resolv_retry_infinite;
+	}
+
 	$uci.$sync("openvpn").done(function(res){
 		$scope.openvpn = $uci.openvpn["@openvpn"][0];
 		//$scope.openvpn.config = $scope.openvpn.auth_user.value+$scope.openvpn.auth_user.value;
