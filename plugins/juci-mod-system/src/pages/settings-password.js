9cb4264e8025c51354b6bfa2eba527ac44693f6d
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2017-09-01 16:27:32 +0200

    juci-mod-system: refactor password page to use new $password factory

--- plugins/juci-mod-system/src/pages/settings-password.js
+++ plugins/juci-mod-system/src/pages/settings-password.js
@@ -19,16 +19,10 @@
  */
 
 JUCI.app
-.controller("SettingsPasswordCtrl", function($scope, $rpc, $tr, gettext, $uci){
-	$scope.showPassword = 0;
-	$scope.showModal = 0;
-	$scope.username = $rpc.$session.data.username;
-	$scope.modal = {
-		old_password: "",
-		password: "",
-		password2: ""
+.controller("SettingsPasswordCtrl", function($scope, $rpc, $password, $tr, gettext, $uci){
+	$scope.model = {
+		username: $rpc.$session.data.username,
 	};
-	$scope.passwordStrength = 1;
 	
 	$uci.$sync("passwords").done(function(){
 		$scope.allUsers = $uci.passwords["@usertype"].map(function(x){
@@ -37,53 +31,13 @@ JUCI.app
 		$scope.$apply();
 	});
 
-	function measureStrength(p) {
-		var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-		var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
-		var enoughRegex = new RegExp("(?=.{4,}).*", "g");
-
-		if(strongRegex.test(p)) return 3;
-		if(mediumRegex.test(p)) return 2;
-		if(enoughRegex.test(p)) return 1;
-		return 0;
-	}
-	
-	$scope.$watch("modal", function(){
-		$scope.passwordStrength = measureStrength($scope.modal.password);
-	}, true);
-	
-	var username = $scope.modal.username = $rpc.$session.data.username;
-	$scope.$watch("modal.username", function(value){
-		if(value == undefined) return;
-		username = value;
-	});
-
 	$scope.onChangePasswordClick = function(){
-		$scope.modal = {};
-		$scope.showModal = 1;
+		$password.change(true, $scope.model.username);
 	}
 
 	$scope.onResetPasswordClick = function(){
-		$uci.passwords[username].reset.value = 1;
+		$uci.passwords[$scope.model.username].reset.value = 1;
 	}
 
-	$scope.onAcceptModal = function(){
-		$scope.error = "";
-		if($scope.modal.password != $scope.modal.password2) alert($tr(gettext("Passwords do not match!")));
-		else {
-			$rpc.$call("router.system", "password_set", {user: username, password: $scope.modal.password, curpass: $scope.modal.old_password}).done(function(data){
-				$scope.showModal = 0;
-				$scope.error = 0;
-				$scope.$apply();
-			}).fail(function(data){
-				$scope.error = $tr(gettext("Was unable to set password. Please make sure you have entered correct current password!"));
-				$scope.$apply();
-			});
-		}
-	}
-	$scope.onDismissModal = function(){
-		$scope.error = 0;
-		$scope.showModal = 0;
-	}
 });
 
