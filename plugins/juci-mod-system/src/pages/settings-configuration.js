a8af212ed2c40078e75bd9995eb7708fc98b4e46
Ionut-Alex Oprea <ionutalexoprea@gmail.com>
2018-01-22 13:53:36 +0100

    juci: soft reset: hide section on repeaters refs #13673

--- plugins/juci-mod-system/src/pages/settings-configuration.js
+++ plugins/juci-mod-system/src/pages/settings-configuration.js
@@ -17,7 +17,7 @@
  */
 
 JUCI.app
-.controller("SettingsConfigurationCtrl", function($scope, $rpc, $tr, gettext, $juciDialog, $file, $config, $events, $juciConfirm){
+.controller("SettingsConfigurationCtrl", function($scope, $rpc, $tr, gettext, $juciDialog, $file, $config, $events, $juciConfirm, $uci){
 	$scope.data = {encryptBackup:false};
 	$scope.sessionID = $rpc.$sid();
 	$scope.resetPossible = 0;
@@ -31,6 +31,7 @@ JUCI.app
 	});
 
 	$scope.soft = {
+		is_repeater: true,
 		soft: false,
 		wifi: true,
 		fw_redirect: true,
@@ -38,6 +39,36 @@ JUCI.app
 		passwd_user: true,
 		ice: true
 	};
+
+	/* set is_repeater
+	 * use is_repeater to hide soft reset section
+	 * should be removed when soft reset will work for repeaters
+	 */
+	$uci.$sync("netmode")
+		.done(function() {
+			if (!$uci.netmode.setup || !$uci.netmode.setup.curmode) {
+				$scope.soft.is_repeater = false;
+				$scope.$apply();
+				return;
+			}
+
+			var curmode = $uci.netmode.setup.curmode.value;
+
+			if (!$uci.netmode[curmode] || !$uci.netmode[curmode].uplink_band) {
+				$scope.soft.is_repeater = false;
+				$scope.$apply();
+				return;
+			}
+
+			var uplinkband= $uci.netmode[curmode].uplink_band.value;
+
+			if (!uplinkband) {
+				$scope.soft.is_repeater = false;
+				$scope.$apply();
+			}
+		}
+	);
+
 
 	$scope.onReset = function(){
 		$juciConfirm.show($tr(gettext("This will reset your configuration to factory defaults (except the settings selected to be saved). Do you want to continue?"))).done(function(){
