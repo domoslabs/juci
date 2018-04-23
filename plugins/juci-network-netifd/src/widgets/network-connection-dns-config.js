f919fbcb97bc9794b86041f8a2f31b0d90eedecc
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2018-04-23 17:47:09 +0200

    add custom ipv6 dns-servers on connections ref #14671

--- plugins/juci-network-netifd/src/widgets/network-connection-dns-config.js
+++ plugins/juci-network-netifd/src/widgets/network-connection-dns-config.js
@@ -12,37 +12,52 @@ JUCI.app
 		require: "^ngModel"
 	};
 })
-.controller("networkConnectionDnsConfig", function($scope, $uci, $network, $log, gettext, $tr){
-	$scope.data = [];
+.controller("networkConnectionDnsConfig", function($scope, $uci, gettext, $tr){
+	$scope.dnsServers = [];
 	$scope.$watch("interface", function(){
-		if(!$scope.interface || !$scope.interface.dns) return;
-		$scope.data = [];
+		if(!$scope.interface || !$scope.interface.dns)
+			return;
+		$scope.dnsServers = [];
 		$scope.interface.dns.value.map(function(dns){
 			dns.split(" ").map(function(d){
-				$scope.data.push({ value:d});
+				$scope.dnsServers.push({ value:d });
 			});
 		});
+
 		$scope.interface.dns.validator = new dnsValidator;
+
+		$scope.$watch("dnsServers", function(){
+			if(!$scope.dnsServers || !$scope.interface || !$scope.interface.dns) return;
+			$scope.interface.dns.value = $scope.dnsServers.map(function(x){ return x.value});
+		}, true);
 	}, false);
+
 	var ipv4validator = new $uci.validators.IP4AddressValidator;
+	var ipv6validator = new $uci.validators.IP6AddressValidator;
+
 	function dnsValidator(){
 		this.validate = function(data){
 			if(data.value.find(function(dns){
-				if( ipv4validator.validate({value:dns}) != null) return true;
+				if( ipv4validator.validate({value:dns}) != null
+				 && ipv6validator.validate({value:dns}) != null) return true;
 				return false;
 			}) != undefined) return $tr(gettext("All DNS-servers must be valid"));
-			if(duplicatesInData()) return $tr(gettext("All DNS-servers must be unique"));
+			if(duplicatesInDnsServers()) return $tr(gettext("All DNS-servers must be unique"));
 			return null;
 		};
 	}
-	$scope.addDns = function(){ $scope.data.push({ value: ""})};
-	$scope.removeDns = function(index){ if($scope.data[index]) $scope.data.splice(index, 1);};
-	$scope.$watch("data", function(){
-		if(!$scope.data || !$scope.interface || !$scope.interface.dns) return;
-		$scope.interface.dns.value = $scope.data.map(function(x){ return x.value});
-	}, true);
-	function duplicatesInData(){
-		var dnslist = $scope.data.map(function(x){ return x.value;});
+
+	$scope.addDns = function(){
+		$scope.dnsServers.push({ value: ""})
+	}
+
+	$scope.removeDns = function(index){
+		if($scope.dnsServers[index])
+			$scope.dnsServers.splice(index, 1);
+	}
+
+	function duplicatesInDnsServers(){
+		var dnslist = $scope.dnsServers.map(function(x){ return x.value;});
 		var sorted_list = dnslist.sort();
 		for(var i = 0; i < sorted_list.length -1; i++){
 			if(sorted_list[i+1] == sorted_list[i]) return true;
