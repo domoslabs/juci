d1ed7848cf57370efa9764989b230ba197f41ecf
Jakob Olsson <jakob.olsson@iopsys.eu>
2018-10-19 16:07:52 +0200

    juci-wifilife: minor style fixes

--- plugins/juci-wifilife/src/pages/wifilife.js
+++ plugins/juci-wifilife/src/pages/wifilife.js
@@ -2,31 +2,28 @@ JUCI.app.
 controller("wifilife", function ($scope, $rpc, $tr, $uci, $wifilife) {
 
 	function populateEntry (section) {
-		if (section.priority.value != null) {
+		if (section.priority.value != null)
 			section.$statusList.push({ label: $tr(gettext("Priority")), value: section.priority.value })
-		}
-		if (section.rssi_threshold.value != null) {
+
+		if (section.rssi_threshold.value != null)
 			section.$statusList.push({ label: $tr(gettext("Threshold")), value: section.rssi_threshold.value + " dBm" })
-		}
-		if (section.bssload_threshold.value != null) {
+
+		if (section.bssload_threshold.value != null)
 			section.$statusList.push({ label: $tr(gettext("Threshold")), value: section.bssload_threshold.value + " %" })
-		}
-		if (section.threshold_margin.value != null) {
+
+		if (section.threshold_margin.value != null)
 			section.$statusList.push({ label: $tr(gettext("Threshold Margin")), value: "± " + section.threshold_margin.value + " db"})
-		}
-		if (section.hysteresis.value != null) {
+
+		if (section.hysteresis.value != null)
 			section.$statusList.push({ label: $tr(gettext("Hysteresis")), value: section.hysteresis.value})
-		}
-		if (section.snr_diff.value != null) {
+
+		if (section.snr_diff.value != null)
 			section.$statusList.push({ label: $tr(gettext("SNR Difference")), value: section.snr_diff.value + " db" })
-		}
 	}
 
 	$uci.$sync("wifilife").done(function () {
 		$scope.wifilife = $uci.wifilife["@wifilife"][0];
 		$scope.steer = $uci.wifilife["@steer"][0];
-		//$scope.$wifilife
-		console.log("481");
 
 		$scope.params = [];
 
@@ -36,17 +33,6 @@ controller("wifilife", function ($scope, $rpc, $tr, $uci, $wifilife) {
 
 			populateEntry(section);
 			$scope.params.push(section);
-			/*for (let key in section) {
-				console.log("14", key, ", ",  section[key]);
-				if (key.charAt(0).toUpperCase() === key.charAt(0).toLowerCase())
-					continue;
-				console.log(key);
-				if (section[key].value == null)
-					continue;
-				section.$statusList.push({ label: $tr(gettext(key.charAt(0).toUpperCase() + key.substr(1))), value: section[key].value});
-			}
-			$scope.params.push(section);
-			console.log(section);*/
 		});
 	});
 
