2bc0afae0794bc51656012f3a372bb267ceb2608
Jakob Olsson <jakob.olsson@iopsys.eu>
2019-04-04 11:57:52 +0200

    juci-easyqos: set applicable dvalues

--- plugins/juci-easyqos/src/juci-easyqos.js
+++ plugins/juci-easyqos/src/juci-easyqos.js
@@ -1,8 +1,8 @@
 UCI.$registerConfig("easy_qos"); 
 UCI.easy_qos.$registerSectionType("rule", {
-	"priority":		{ dvalue: "", type: String },
+	"priority":		{ dvalue: "normal", type: String },
 	"macaddr":		{ dvalue: "", type: String },
-	"proto":		{ dvalue: "", type: String },
-	"port":		{ dvalue: [], type: Array },
+	"proto":		{ dvalue: "all", type: String },
+	"port": 		{ dvalue: [], type: Array },
 	"comment":		{ dvalue: "NA", type: String }
-}); 
+});
