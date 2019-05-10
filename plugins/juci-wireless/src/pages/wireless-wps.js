0b29e24ea4d256867117497486e9175d20b53d0b
Jakob Olsson <jakob.olsson@iopsys.eu>
2019-05-10 15:01:14 +0200

    juci-wireless: allow enabling wps on a blacklist basis

--- plugins/juci-wireless/src/pages/wireless-wps.js
+++ plugins/juci-wireless/src/pages/wireless-wps.js
@@ -96,7 +96,7 @@ JUCI.app
 	$scope.progress = 0;
 	
 	$scope.wpsUnlocked = function(interface){
-		return ["none", "psk2", "mixed-psk"].indexOf(interface.encryption.value) !== -1 && interface.hidden.value === false;
+		return ["none", "wep"].indexOf(interface.encryption.value) === -1 && interface.hidden.value === false;
 	}
 	
 	$wireless.getInterfaces().done(function(ifaces){
