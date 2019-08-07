domoslabs/juci

commit f8784b14acafd1a09fc59de55cd64cf8d91b7bf6

juci-netmode: disable profile overview widget btn if no profiles

plugins/juci-netmode/src/widgets/overview-netmode.js
@@ -78,7 +78,7 @@ JUCI.app
 	};
 
 	$scope.onChangeModeConfirm = function(){
-		if(!$scope.data || !$scope.setup || !$scope.setup.curmode) return;
+		if(!$scope.data || !$scope.data.length ||  !$scope.setup || !$scope.setup.curmode) return;
 		if($scope.data.selected === $scope.setup.curmode.value) return;
 		$juciConfirm.show($tr(gettext("Changing netmode will reset your configuration completely to match that netmode. Do you want to continue?"))).done(function(value){
 			setNetmode($scope.data.selected);

commit 7528e609e8117e16e11d566eb06db7fa613f3989

juci-netmode: Fix bug introduced by f8784b14acafd1a09fc59de55cd64cf8d91b7bf6

plugins/juci-netmode/src/widgets/overview-netmode.js
@@ -78,7 +78,7 @@ JUCI.app
 	};
 
 	$scope.onChangeModeConfirm = function(){
-		if(!$scope.data || !$scope.data.length ||  !$scope.setup || !$scope.setup.curmode) return;
+		if(!$scope.data || $scope.allNetmodes.length < 1 ||  !$scope.setup || !$scope.setup.curmode) return;
 		if($scope.data.selected === $scope.setup.curmode.value) return;
 		$juciConfirm.show($tr(gettext("Changing netmode will reset your configuration completely to match that netmode. Do you want to continue?"))).done(function(value){
 			setNetmode($scope.data.selected);